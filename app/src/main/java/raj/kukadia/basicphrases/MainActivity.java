d68f994accb182bc40767e03fa08542153e557d3
Raj <rajkukadia1@gmail.com>
2018-08-14 00:17:27 -0400

    arabic

--- app/src/main/java/raj/kukadia/basicphrases/MainActivity.java
+++ app/src/main/java/raj/kukadia/basicphrases/MainActivity.java
@@ -1,7 +1,10 @@
 package raj.kukadia.basicphrases;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,8 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
 
-        Button button = new Button();
 
+    public void buttonTapped(View view){
+         int id = view.getId();
+         int resourceId = 0;
+         String ourId = "";
+         ourId = view.getResources().getResourceEntryName(id);
+         resourceId = getResources().getIdentifier("arabic_"+ourId, "raw", "raj.kukadia.basicphrases" );
+          //  if(id == ""){
+              //  resourceId =
+            //}
+               // Log.d(ourId, "check");
+         //int resourceId = getResources().getIdentifier(ourId)
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, resourceId);
+            mediaPlayer.start();
     }
 }
